2748d0d528b78b1c9beafa0fd58cb0339ca2ac3c
Chris Dillon <squarism@gmail.com>
2017-08-15 01:14:09 -0700

    Simplify loop and support fading brightness.
    * Reduce load on particle by getting rid of breathing.
    * Code formatting.
    * Move the off branch somewhere else, causing weirdness and bugs.
    * All colors are declared ahead of time.
    * Basically empty the main loop() too.  Idk why I was strcmp() constantly. :(

--- blinkwon.ino
+++ blinkwon.ino
@@ -4,10 +4,10 @@
 // This #include statement was automatically added by the Particle IDE.
 #include <neopixel.h>
 
-// Hey!
-// Particle IDE put these include lines in but you still have to to do the "Add to App" thing.
+// Hey!  Particle IDE put these include lines in but you still have to to do the "Add to App" thing.
 #include <SparkJson.h>
 #include <neopixel.h>
+#define PIXEL_TYPE WS2812B
 
 
 // ---- CHANGE THIS ----
@@ -15,7 +15,6 @@
 #define LEDS 24
 
 
-#define PIXEL_TYPE WS2812B
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, PIN, PIXEL_TYPE);
 
 // status light colors
@@ -24,128 +23,131 @@ int colorEnd = 255;
 int colorRotationSlowness = defaultColorRotationSpeed();
 int brightness = defaultBrightness();
 
-char *theme = "unknown";
+char * theme = "unknown";
 
 uint32_t white = strip.Color(255, 255, 255);
 uint32_t black = strip.Color(0, 0, 0);
-
+uint32_t green = strip.Color(45, 210, 0);
+uint32_t red = strip.Color(240, 15, 0);
+uint32_t lime = strip.Color(105, 150, 80);
+uint32_t orange = strip.Color(180, 75, 0);
+uint32_t blacklight = strip.Color(105, 0, 150);
+uint32_t yellow = strip.Color(150, 105, 0);
+uint32_t blue = strip.Color(30, 0, 225);
+uint32_t bluegreen = strip.Color(0, 90, 165);
+uint32_t pink = strip.Color(230, 0, 25);
 
 void setup() {
-  strip.begin();
-  strip.setBrightness(brightness);
-  strip.show(); // Initialize all pixels to 'off'
-
-  // subscribe to private events - this finally seems to work like I want.
-  Particle.subscribe("squarism/blinkwon", eventHandler, MY_DEVICES);
+    strip.begin();
+    strip.setBrightness(brightness);
+    strip.show(); // Initialize all pixels to 'off'
+    
+    // subscribe to private events - this finally seems to work like I want.
+    Particle.subscribe("squarism/blinkwon", eventHandler, MY_DEVICES);
 }
 
-
 void loop() {
-  // we don't need to call process or connect here
-  // the particle defaults to automatic mode.
-  // more here about it: http://blog.particle.io/2014/08/06/control-the-connection/
-
-  if (strcmp(theme, "unknown") == 0 ) {
-    // idle();
-  }
-    
-  if (strcmp(theme, "cylon") == 0 ) {
-    colorStart = 0;
-    colorEnd = 255;
-    strip.setBrightness(120);
-    dotChase(90, 45, 3);
-  }
+    // we don't need to call process or connect here
+    // the particle defaults to automatic mode.
+    // more here about it: http://blog.particle.io/2014/08/06/control-the-connection/
 }
 
 int defaultColorRotationSpeed() {
-   return 6000; 
+    return 6000;
 }
 
 int defaultBrightness() {
     return 120;
 }
 
-void eventHandler(const char *event, const char *data) {
+void eventHandler(const char * event, const char * data) {
     int len = strlen(data);
-    char dataCopy[len+1];
+    char dataCopy[len + 1];
     strcpy(dataCopy, data);
 
-
     StaticJsonBuffer<500> jsonBuffer;
-    JsonObject& json = jsonBuffer.parseObject(dataCopy);
-    
+    JsonObject & json = jsonBuffer.parseObject(dataCopy);
+
     if (json.success()) {
-      if (json.containsKey("brightness"))
-      {
-        brightness = json["brightness"];
-        strip.setBrightness(brightness);
-        strip.show();
-      }
-    
-      if ( json.containsKey("theme"))
-      {
-        theme = const_cast<char*>(json["theme"].asString());
-        setColor(theme);
-      } 
-    
-      if (json.containsKey("idle"))
-      {
-        theme = "unknown";
-      }
+        if (json.containsKey("brightness")) {
+            int oldBrightness = brightness;
+            brightness = json["brightness"];
+            tweenToBrightness(oldBrightness, (int)brightness, 90);
+        }
+
+        if (json.containsKey("theme")) {
+            theme = const_cast <char*> (json["theme"].asString());
+            if (strcmp(theme, "off") == 0) {
+                tweenToBrightness((int)brightness, 0, 90);
+                strip.setBrightness(0);
+                strip.show();
+                brightness = 0;
+            } else {
+                setColor(theme);
+            }
+        }
+
+        if (json.containsKey("idle")) {
+            theme = "unknown";
+        }
     }
 }
 
-void setColor(const char *theme) {
+void setColor(const char * theme) {
     colorRotationSlowness = defaultColorRotationSpeed();
-    
+
     // wow, you can't easily do switch statements in C?
-    if (strcmp(theme, "green") == 0) {
-        colorStart = 15;
-        colorEnd = 35;
-        colorRing(colorRotationSlowness);
-    } else if (strcmp(theme, "lime") == 0) {
-        colorStart = 35;
-        colorEnd = 45;
-        colorRing(colorRotationSlowness);
+    if (strcmp(theme, "bluegreen") == 0) {
+        colorWipe(bluegreen, 15);
+    } else if (strcmp(theme, "white") == 0) {
+        colorWipe(white, 15);
+    } else if (strcmp(theme, "blacklight") == 0) {
+        colorWipe(blacklight, 15);
+    } else if (strcmp(theme, "orange") == 0) {
+        colorWipe(orange, 15);
+    } else if (strcmp(theme, "yellow") == 0) {
+        colorWipe(yellow, 15);
     } else if (strcmp(theme, "red") == 0) {
-        colorStart = 80;
-        colorEnd = 95;
-        colorRing(colorRotationSlowness);
-    } else if (strcmp(theme, "blacklight") == 0 ) {
-        colorStart = 135;
-        colorEnd = 160;
-        colorRing(colorRotationSlowness);
-    } else if (strcmp(theme, "yellow") == 0 ) {
-        colorStart = 50;
-        colorEnd = 70;
-        colorRing(colorRotationSlowness);
-    } else if (strcmp(theme, "blue") == 0 ) {
-        colorStart = 160;
-        colorEnd = 180;
-        colorRing(colorRotationSlowness);
-    } else if (strcmp(theme, "bluegreen") == 0 ) {
-        colorStart = 200;
-        colorEnd = 220;
-        colorRing(colorRotationSlowness);
-    } else if (strcmp(theme, "orange") == 0 ) {
-        colorStart = 60;
-        colorEnd = 80;
-        colorRing(colorRotationSlowness);
-    } else if (strcmp(theme, "pink") == 0 ) {
-        colorStart = 90;
-        colorEnd = 110;
-        colorRing(colorRotationSlowness);
-    } else if (strcmp(theme, "rainbow") == 0 ) {
+        colorWipe(red, 15);
+    } else if (strcmp(theme, "lime") == 0) {
+        colorWipe(lime, 15);
+    } else if (strcmp(theme, "blue") == 0) {
+        colorWipe(blue, 15);
+    } else if (strcmp(theme, "green") == 0) {
+        colorWipe(green, 15);
+    } else if (strcmp(theme, "pink") == 0) {
+        colorWipe(pink, 15);
+    } else if (strcmp(theme, "rainbow") == 0) {
         colorStart = 0;
         colorEnd = 255;
         colorRing(colorRotationSlowness);
-    } else if (strcmp(theme, "off") == 0 ) {
-        colorWipe(black, 80);
-        strip.setBrightness(0);
-        strip.show();
-    } else if (strcmp(theme, "white") == 0 ) {
-        colorWipe(white, 0);
+    } else if (strcmp(theme, "cylon") == 0) {
+        colorStart = 0;
+        colorEnd = 255;
+        strip.setBrightness(120);
+        dotChase(90, 45, 3);
     }
+}
+
+// Really tried to DRY this out with clever code, but couldn't get it.
+void tweenToBrightness(int from, int to, uint8_t wait) {
+    if (from == to) { return; }
+
+    if (from < to) {
+        for (int i = from; i < to; i++) {
+            strip.setBrightness(i);
+            strip.show();
+            delay(wait);
+        }
+    } else {
+        for (int i = from; i > to; i--) {
+            strip.setBrightness(i);
+            strip.show();
+            delay(wait);
+        }
+    }
+
+    brightness = to;
 }
 
 void idle() {
@@ -153,77 +155,63 @@ void idle() {
     dotChase(245, 35, 2);
 }
 
-// This doesn't work at all.  Need to guard with a boolean maybe?
-void fadeOut(uint8_t wait) {
-    for(uint8_t i=brightness; i >= 0; i--) {
-        strip.setBrightness(i);
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+    for (uint16_t i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i, c);
         strip.show();
         delay(wait);
     }
-    strip.setBrightness(0);
-    strip.show();
-}
-
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-      strip.show();
-      delay(wait);
-  }
 }
 
 void colorRing(uint16_t wait) {
-  uint16_t i, j;
-  for(j=colorStart; j<colorEnd; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(j & 255));
+    uint16_t i, j;
+    for (j = colorStart; j < colorEnd; j++) {
+        for (i = 0; i < strip.numPixels(); i++) {
+            strip.setPixelColor(i, Wheel(j & 255));
+        }
+        strip.show();
+        delay(wait);
     }
-    strip.show();
-    delay(wait);
-  }
-  for(j=colorEnd; j>colorStart; j--) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(j & 255));
+    for (j = colorEnd; j > colorStart; j--) {
+        for (i = 0; i < strip.numPixels(); i++) {
+            strip.setPixelColor(i, Wheel(j & 255));
+        }
+        strip.show();
+        delay(wait);
     }
-    strip.show();
-    delay(wait);
-  }
-
 }
-
 
 // dot chase visual effect
 void dotChase(uint32_t c, uint8_t wait, int dots) {
-  int offset = LEDS / dots;  // pixel separation
-  uint32_t color = Wheel(c & 255);
+    int offset = LEDS / dots; // pixel separation
+    uint32_t color = Wheel(c & 255);
 
-  for (int q=0; q < offset; q++) {
-    for (int i=0; i < strip.numPixels(); i=i+offset) {
-      strip.setPixelColor(i+q, color);    //turn every third pixel on
+    for (int q = 0; q < offset; q++) {
+        for (int i = 0; i < strip.numPixels(); i = i + offset) {
+            strip.setPixelColor(i + q, color); // turn every third pixel on
+        }
+
+        strip.show();
+        delay(wait);
+
+        for (int i = 0; i < strip.numPixels(); i = i + offset) {
+            strip.setPixelColor(i + q, 0); // turn every n-offset pixel off
+        }
+
     }
-
-    strip.show();
-    delay(wait);
-
-    for (int i=0; i < strip.numPixels(); i=i+offset) {
-      strip.setPixelColor(i+q, 0);        //turn every n-offset pixel off
-    }
-  
-   }
 }
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
+    if (WheelPos < 85) {
+        return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+    } else if (WheelPos < 170) {
+        WheelPos -= 85;
+        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    } else {
+        WheelPos -= 170;
+        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    }
 }
-
